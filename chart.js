b067c0bc8fe39c1cb929fa27e3a9d1b53ae7eb92
ASilva Innovations <106929764+asilva777@users.noreply.github.com>
2025-07-21 22:30:24 +0800

    chart.js
    Signed-off-by: ASilva Innovations <106929764+asilva777@users.noreply.github.com>

---
+++ chart.js
@@ -0,0 +1,9 @@
+const moodData = {
+  labels: ["Jul 1", "Jul 5", "Jul 9", "Jul 13", "Jul 17", "Jul 21", "Jul 25", "Jul 29"],
+  datasets: [
+    { label: "Red", data: [2, 3, 1, 4, 2, 2, 3, 2], backgroundColor: "#e53935" },
+    { label: "Blue", data: [3, 2, 3, 1, 2, 4, 3, 1], backgroundColor: "#1e88e5" },
+    { label: "Yellow", data: [1, 2, 4, 3, 2, 2, 1, 4], backgroundColor: "#fdd835" },
+    { label: "Green", data: [2, 1, 2, 2, 3, 1, 2, 2], backgroundColor: "#43a047" }
+  ]
+};
